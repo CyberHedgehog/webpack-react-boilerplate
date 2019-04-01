3a803f4489e97516e64e5389a17751472d0a63e1
Dmitriy Dobrenkiy <reg.dobr@live.ru>
2019-04-01 16:36:00 +0300

    working boilerplate

---
+++ Readme.md
@@ -0,0 +1,9 @@
+#Webpack + ReactJS boilerplate
+
+##Modules:
+* babel-loader
+* css-loader
+
+##Webpack packages:
+* cli
+* dev-server
---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import '../styles/App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from react with working jsx!</h1>
+                <h2>And hello from dev-server!</h2>
+            </div>
+        );
+    }
+}
+
+export default App;
